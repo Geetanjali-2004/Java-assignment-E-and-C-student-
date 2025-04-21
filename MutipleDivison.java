42461d27a25a881a44e5ba3fd8fcfce84809161b
Geetanjali patil <geetanjalipatil818@gmail.com>
2025-04-21 11:56:09 +0530

    Add files via upload

---
+++ MutipleDivison.java
@@ -0,0 +1,17 @@
+package Pack1;
+
+public class MutipleDivison {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int c=a<<2;
+		System.out.println(c);
+		int d=a>>2;
+		System.out.println(d);
+		
+		
+
+	}
+
+}
