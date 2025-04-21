10fd16ead330b54fda284bc1585337be80782cab
Geetanjali patil <geetanjalipatil818@gmail.com>
2025-04-21 11:50:30 +0530

    Add files via upload

---
+++ Fabonnicca.java
@@ -0,0 +1,17 @@
+package Pack1;
+
+public class Fabonnicca {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =0; 
+    while(a<=10)
+    {
+    a++;
+    	int sum =0;
+    	int sum1=a+sum;
+    	a++;
+    	System.out.println(sum1);
+    }
+	}
+}
